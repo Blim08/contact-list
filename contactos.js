e847a52ef0b4b024782de7a17a017bc4f8b376aa
Jairo Durango <cartuchoxl@hotmail.com>
2023-02-06 17:01:11 -0500

    creada lista de contactos usando objetos

--- contactos.js
+++ contactos.js
@@ -1,17 +1,32 @@
-let misAmigos = ['Martin Arrieta','German Forero','Diego Florez', 'Jafet Rodriguez']
+let misAmigos = []
 
-function agregarContactos(contacto) {
-    misContactos.push(contacto)
+function agregarContacto(id, nombres, apellidos, telefono, ciudad, direccion) {
+    let contacto = {
+        id: id,
+        nombres: nombres,
+        apellidos: apellidos,
+        telefono: telefono,
+        ubicaciones: {
+            ciudad:ciudad,
+            direccion: direccion
+        }
+    } 
+    misAmigos.push(contacto);    
 }
 
-
-function suprimir(contacto){
-    let nuevaLista = misAmigos.filter((persona) => persona !== contacto )
+function eliminarContacto(id){
+    let nuevaLista = misAmigos.filter((persona) => persona.id !== id  )
     misAmigos = nuevaLista;
 }
 
 
 function mostrarAmigos(){
-    misAmigos.sort()
     misAmigos.forEach((contacto) => console.log(contacto)); 
 }
+
+
+agregarContacto(0,"Erika","perez","3103965967","monteria","los cordobas")
+agregarContacto(1,"jasmin","yerena","3103964578","Antioquia","Turbo")
+agregarContacto(2,"Marcela","perez","3103965967","monteria","los cordobas")
+eliminarContacto(1)
+mostrarAmigos()

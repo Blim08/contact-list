58f0138e478ec37369e75dfec1bceaae85f1a061
Jairo Durango <cartuchoxl@hotmail.com>
2023-02-06 21:58:25 -0500

    creada funcionalidad bonus de actualizar contacto

--- contactos.js
+++ contactos.js
@@ -1,17 +1,51 @@
-let misAmigos = ['Martin Arrieta','German Forero','Diego Florez', 'Jafet Rodriguez']
+let misAmigos = []
 
-function agregarContactos(contacto) {
-    misContactos.push(contacto)
+function agregarContacto(id, nombres, apellidos, telefono, ciudad, direccion) {
+    let contacto = {
+        id: id,
+        nombres: nombres,
+        apellidos: apellidos,
+        telefono: telefono,
+        ubicaciones: {
+            ciudad:ciudad,
+            direccion: direccion
+        }
+    } 
+    misAmigos.push(contacto);    
 }
 
+function actualizarContacto(id, nuevoNombres, nuevoApellidos, nuevoTelefono, nuevaCiudad, nuevaDireccion) {
+    for (let i = 0; i < misAmigos.length; i++) {
+        if (misAmigos[i].id === id) {
+           misAmigos[i] = {
+                id,
+                nombres: nuevoNombres, 
+                apellidos: nuevoApellidos,
+                telefono : nuevoTelefono,
+                ubicaciones: {
+                    ciudad:  nuevaCiudad,
+                    direccion: nuevaDireccion
+                }
+           } 
+        }
+    }
+}
 
-function suprimir(contacto){
-    let nuevaLista = misAmigos.filter((persona) => persona !== contacto )
+function eliminarContacto(id){
+    let nuevaLista = misAmigos.filter((persona) => persona.id !== id  )
     misAmigos = nuevaLista;
 }
 
 
 function mostrarAmigos(){
-    misAmigos.sort()
     misAmigos.forEach((contacto) => console.log(contacto)); 
 }
+
+
+agregarContacto(0,"Erika","perez","3103965967","monteria","los cordobas")
+agregarContacto(1,"jasmin","yerena","3103964578","Antioquia","Turbo")
+agregarContacto(2,"Marcela","perez","3103965967","monteria","los cordobas")
+eliminarContacto(1)
+mostrarAmigos()
+actualizarContacto(2,"Marcela","Benitez","3103965967","monteria","las florez")
+mostrarAmigos()
